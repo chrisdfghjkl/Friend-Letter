5009eebe5287b6766d97f453ef2329878aa04dee
chrisdfghjkl <cdepastene@gmail.com>
2022-02-13 10:58:53 -0800

    add constructor

--- FriendLetter/Startup.cs
+++ FriendLetter/Startup.cs
@@ -8,6 +8,15 @@ namespace FriendLetter
 {
   public class Startup
   {
+    public Startup(IWebHostEnvireonment env)
+    {
+      var builder = new ConfigurationBuilder()
+        .SetBasePath(env.ContentRootPath)
+        .AddEnvironmentVariables();
+      Configuration = builder.Build();
+    }
 
+    public IConfigurationRoot Configuration {get;}
+    
   }
 }
